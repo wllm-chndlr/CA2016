a09d6f7874d74e5380c8b11a208cc2adebfdb260
William Chandler <chndlr.wllm@gmail.com>
2020-08-19 14:43:39 -0500

    cleanup, removed previous iterations and unneeded assets

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# CA2016
+# California 2016
+
+### Flex panel photo display with dynamic text
+
+##### Built with vanilla JS/CSS
+
+![screenshot](assets/images/preview.png)
--- assets/scripts/main2.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('p').mouseenter(function(){
-        $('p').fadeTo('fast', 1.00);
-    });
-    $('p').mouseleave(function(){
-        $('p').fadeTo('fast', 0.5);
-    });
-});
